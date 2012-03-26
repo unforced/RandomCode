64523405f6fffe2de318a075fec14c572a064de8
Aaron Neyer <aaron@aaronneyer.com>
2012-03-25 22:31:02 -0400

    Added some error checking

--- uncertainty.py
+++ uncertainty.py
@@ -1,22 +1,28 @@
 import parser
 def calculateUncertainty(variables, uncertainties, formula):
-    if (len(uncertainties) != len(variables)): print 'fail'
-    uncertaintyDueTo = []
-    temp = variables[:]
-    original = findValue(variables,formula)
-    for x in range(len(temp)):
-        temp[x] += uncertainties[x]
-        newValue = findValue(temp,formula)
-        uncertaintyDueTo.append(newValue-original)
-        temp[x] -= uncertainties[x]
-    totalUncertainty = 0
-    for x in range(len(uncertaintyDueTo)):
-        print "Uncertainty due to {} is {}".format(x, uncertaintyDueTo[x])
-        totalUncertainty += uncertaintyDueTo[x]**2
-    print "Total Uncertainty is {}".format(totalUncertainty**(0.5))
+    if (len(uncertainties) != len(variables)): 
+        print 'Please input same number of variables as uncertainties'
+    else:
+        for x in range(len(variables)):
+            if 'n[{}]'.format(x) not in formula:
+                print 'Please use all variables in the formula'
+        uncertaintyDueTo = []
+        temp = variables[:]
+        original = findValue(variables,formula)
+        for x in range(len(temp)):
+            temp[x] += uncertainties[x]
+            newValue = findValue(temp,formula)
+            uncertaintyDueTo.append(newValue-original)
+            temp[x] -= uncertainties[x]
+        totalUncertainty = 0
+        for x in range(len(uncertaintyDueTo)):
+            print "Uncertainty due to {} is {}".format(x, uncertaintyDueTo[x])
+            totalUncertainty += uncertaintyDueTo[x]**2
+        print "Total Uncertainty is {}".format(totalUncertainty**(0.5))
 
 
 def findValue(numbers,formula):
     code = parser.expr(formula).compile()
     n = numbers[:]
     return eval(code)
+    #Still needs error checking for formula name errors.  Might add later.
